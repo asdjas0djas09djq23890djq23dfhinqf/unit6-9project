709d2130445913db2837f2cb4cf5f12d564e30d6
Rizwan <rizwanfaruqi7@gmail.com>
2025-04-05 18:22:00 -0400

    Object now only has one cycle.

--- src/GameLogic.java
+++ src/GameLogic.java
@@ -14,33 +14,19 @@ public class GameLogic extends Frame {
     Timer timer;
     Frame frame;
     TimerTask throwFruit;
-
     public GameLogic() {
         moves = 0;
-
         frame = new Frame();
         timer = new Timer();
-
-
         throwFruit = new TimerTask() {
             public void run() {
                 decideFruitAndThrow();
             }
         };
-
         timer.schedule(throwFruit, 10);
     }
 
-//    private static Clip loadSound(String soundFilePath) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
-//        AudioInputStream audioIn = AudioSystem.getAudioInputStream(new File(soundFilePath));
-//        Clip clip = AudioSystem.getClip();
-//        clip.open(audioIn);
-//        return clip;
-//    }
-
-
-
-    public void decideFruitAndThrow() {
+     public void decideFruitAndThrow() {
         int level = 3;
         int nextCooldown = (int) (Math.random() * 1500 + 1500);
         int amount = (int) (Math.random() * 4 + 2);
@@ -64,10 +50,13 @@ public class GameLogic extends Frame {
     }
 
     public void throwObject() {
-        ThrowingObject object = new ThrowingObject("images/Apple.png", frame);
-        object.setSize(frame.getWidth(), frame.getHeight());
-        getPanel().add(object);
-        getPanel().repaint();
-        getPanel().revalidate();
+        SwingUtilities.invokeLater(() -> {
+            ThrowingObject object = new ThrowingObject("images/Apple.png", frame.getFrame());
+            object.setBounds(0, 0, 180, 180);
+            if (frame.getPanel() != null) {
+                frame.getPanel().add(object);
+                frame.getPanel().repaint();
+            }
+        });
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 
- class Main {
+class Main {
     public static void main(String[] args) {
         GameLogic game = new GameLogic();
     }
